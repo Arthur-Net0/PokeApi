b162874324d2daf66516f940e872b42fd632f44f
Arthur-Net0 <90418721+Arthur-Net0@users.noreply.github.com>
2022-01-25 17:31:09 -0300

    new code changes

--- src/app/features/pokemon/visor/visor.component.ts
+++ src/app/features/pokemon/visor/visor.component.ts
@@ -13,83 +13,81 @@ import { PokemonResolver } from '../pokemon-routing.module';
 @Component({
   selector: 'app-visor',
   templateUrl: './visor.component.html',
-  styleUrls: ['./visor.component.css']
+  styleUrls: ['./visor.component.css'],
 })
 export class VisorComponent implements OnInit {
-
   constructor(
     private activatedRoute: ActivatedRoute,
     private pokemonService: PokemonService,
     private formBuilder: FormBuilder,
     public coreUtils: CoreUtilsService
-    ) { }
+  ) {}
 
   spriteVersions: SpriteLinks[];
   pokeSearch: String;
   formFilter: FormGroup;
-  divImage = "imageins"
+  divImage = 'imagens';
 
   ngOnInit(): void {
     this.formFilter = this.formBuilder.group({
-      shiny: [false],
+      shiny: false,
       gender: '',
-    })
+    });
 
-    let afterGetPokemon = (pokemon:Pokemon) => this.spriteToSpriteLinks(pokemon.sprites)
-
-    this.activatedRoute.data.subscribe( resolvers => afterGetPokemon(resolvers.pokemon))
-
+    let afterGetPokemon = (pokemon: Pokemon) => this.spriteToSpriteLinks(pokemon.sprites);
+    this.activatedRoute.data.subscribe((data) => afterGetPokemon(data.pokemon));
   }
 
   spriteToSpriteLinks(sprite: PokemonSprites): void {
-    let newSpriteLinks: SpriteLinks[] = [];
-    let versions = Object.keys(sprite.versions)
-    for( let key of versions) {
-      let games = Object.keys(sprite.versions[key])
+    const newSpriteLinks: SpriteLinks[] = [];
+    const versions = Object.keys(sprite.versions);
+    const notVersion: string[] = ['icons'];
+    const verifySprite = (sprite: Sprite) => {
+      return sprite.front || sprite.back;
+    };
+    const validVersion = (version: string) => {
+      return notVersion.indexOf(version) === -1;
+    };
+    for (let key of versions) {
+      const games = Object.keys(sprite.versions[key]);
 
-      for(let game of games) {
-        if (game !== 'icons') {
-          let rawSpritesLinks: AllSprites = sprite.versions[key][game]
+      for (let version of games) {
+        const rawSpritesLinks: AllSprites = sprite.versions[key][version];
+        const newSpriteLink: SpriteLinks = this.spriteLinksFromAll(
+          rawSpritesLinks,
+          version
+        );
 
-          let newSpriteLink: SpriteLinks = this.spriteLinksFromAll(rawSpritesLinks, game);
-
-          if (newSpriteLink.default.front || newSpriteLink.default.back ) {
-            newSpriteLinks.push(newSpriteLink);
-          }
-
+        if (validVersion(version) && verifySprite(newSpriteLink.default)) {
+          newSpriteLinks.push(newSpriteLink);
         }
       }
-
-
     }
     this.spriteVersions = newSpriteLinks;
   }
 
-  private spriteLinksFromAll(rawSpriteLink: AllSprites, game: string): SpriteLinks {
-    let spriteVerify: boolean;
-
-    let verifySprite = (sprite: Sprite) => {
-      spriteVerify = Boolean(sprite.front || sprite.back)
-    }
-
+  private spriteLinksFromAll(
+    rawSpriteLink: AllSprites,
+    game: string
+  ): SpriteLinks {
     let defaultSprite: Sprite = {
       front: rawSpriteLink.front_default,
-      back: rawSpriteLink.back_default
+      back: rawSpriteLink.back_default,
     };
 
     let shinySprite: Sprite = {
       front: rawSpriteLink.front_shiny,
-      back: rawSpriteLink.back_shiny
+      back: rawSpriteLink.back_shiny,
     };
 
     let shinyFemaleSprite: Sprite = {
       front: rawSpriteLink.front_shiny_female,
-      back: rawSpriteLink.back_shiny_female
+      back: rawSpriteLink.back_shiny_female,
     };
 
     let femaleSprite: Sprite = {
       front: rawSpriteLink.front_female,
-      back: rawSpriteLink.back_female
+      back: rawSpriteLink.back_female,
     };
 
     let newSpriteLink: SpriteLinks = {
@@ -104,77 +102,52 @@ export class VisorComponent implements OnInit {
   }
 
   reviewShowedSprite() {
-    let filter = this.formFilter.value
-
-    let verifySprite = (sprite: Sprite) => {
-      if (sprite.front !== undefined || sprite.back !== undefined) {
-        return true
-      }
-      else {
-        return false
-      }
-    }
-    let spriteFiltered = (version: SpriteLinks) => {
+    const filter = this.formFilter.value;
+    const isFemale: boolean = filter.gender === 'female';
+    const verifySprite = (sprite: Sprite) => sprite.front || sprite.back;
+    const spriteFiltered = (version: SpriteLinks) => {
       if (filter.shiny) {
-        if ( filter.gender === "female" && verifySprite(version.shiny_female)) {
-          return version.shiny_female
+        if (isFemale && verifySprite(version.shiny_female)) {
+          return version.shiny_female;
+        } else if (verifySprite(version.shiny)) {
+          return version.shiny;
         }
-        else if (verifySprite(version.shiny)){
-          return version.shiny
-        }
-        else {
-          return version.default
-        }
+      } else if (isFemale && verifySprite(version.female)) {
+        return version.female;
       }
-      else {
-        if (filter.gender === "female" && verifySprite(version.female)) {
-          return version.female
-        }
-        else {
-          return version.default
-        }
-      }
-    }
+      return version.default;
+    };
+    const redefineSprite = () => this.spriteVersions.map( version => version.sprite = spriteFiltered(version) );
 
-    if (filter.shiny && filter.gender === 'female') {
-      this.spriteVersions.map( version => version.sprite = spriteFiltered(version))
-    }
-    else if ( filter.shiny ) {
-      this.spriteVersions.map(version => version.sprite = spriteFiltered(version))
-    }
-    else if ( filter.gender === 'female' ) {
-      this.spriteVersions.map(version => version.sprite = spriteFiltered(version))
-    }
-    else {
-      this.spriteVersions.map( version => version.sprite = version.default )
-    }
-
+    redefineSprite();
   }
 
-  revertImage(event) {
-    let images = document.getElementById(this.divImage).children
+  revertImage() {
+    const images = document.getElementById(this.divImage).children;
 
     let verifyOtherImagesSRC = (img: HTMLElement) => {
-      for( let i in Object.keys(images)) {
-        let toVerify = images[i]
-        if (toVerify !== img && Boolean(toVerify.getAttribute('src')) && Boolean(img.getAttribute('src')))  {
-          return true
+      for (let i in Object.keys(images)) {
+        let toVerify = images[i];
+        const verifySRC = (): boolean => {
+          return Boolean(toVerify.getAttribute('src')) && Boolean(img.getAttribute('src'))
+        }
+        if (toVerify !== img && verifySRC()) {
+          return true;
         }
       }
-      return false
-    }
+      return false;
+    };
 
-    for ( let i in Object.keys(images)) {
-      let image = images[i] as HTMLElement
-      if (image.classList.contains('d-none') && verifyOtherImagesSRC(image)) {
-        image.classList.remove('d-none')
-      }
-      else if (!image.classList.contains('d-none') && verifyOtherImagesSRC(image)) {
-        image.classList.add('d-none')
+    for (let i in Object.keys(images)) {
+      const image = images[i] as HTMLElement;
+      const otherImageSRC: boolean = verifyOtherImagesSRC(image)
+      const imageIsHidden = image.classList.contains('d-none')
+
+      if ( imageIsHidden && otherImageSRC) {
+        image.classList.remove('d-none');
+      } else if ( !imageIsHidden && otherImageSRC ) {
+        image.classList.add('d-none');
       }
     }
   }
-
 }
-
-
